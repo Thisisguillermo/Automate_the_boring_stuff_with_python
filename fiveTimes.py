a65eede571c88c4b1c506bb1fd5779804dc1e232
Guillermo <guillermozaandam@gmail.com>
2020-06-01 19:27:42 +0200

    ...

---
+++ fiveTimes.py
@@ -0,0 +1,14 @@
+## print('My name is')
+## for i in range(7):
+##   print('Jimmy Five Times (' + str(i) + ')')
+
+total = 0
+for num in range(101):
+  print(total)
+  total = total + num
+print(total)
+print(num)
+print(total + num)
+
+for i in range(9, 99):
+  print(i)
