c02ad0bbf709729f816247a3b28a33cbf2317ed9
Guillermo <guillermozaandam@gmail.com>
2020-06-01 20:30:18 +0200

    Chapter 2

---
+++ spam.py
@@ -0,0 +1,15 @@
+spam = input()
+if spam == "1":
+  print('Hello')
+elif spam == "2":
+  print('Howdy')
+else:
+  print('Greetings!')
+
+for i in range(1, 11):
+  print(i)
+
+i = 1
+while i < 11:
+  print(i)
+  i += 1
