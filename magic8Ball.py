1a951811b7d03fd8f681f8d01a711c7fb18a5ee1
Guillermo <guillermozaandam@gmail.com>
2020-06-02 17:35:03 +0200

    ...

--- magic8Ball.py
+++ magic8Ball.py
@@ -20,6 +20,6 @@ def getAnswer(answerNumber):
   elif answerNumber == 9:
       return 'Very doubtful'
 
-  r = random.randint(1, 9)
-  fortune = getAnswer(r)
-  print(fortune)
+r = random.randint(1, 9)
+fortune = getAnswer(r)
+print(fortune)
