5c237c02bf7a6ce8003135bce246e5ebe1785578
Guillermo <guillermozaandam@gmail.com>
2020-06-02 17:09:06 +0200

    ...

--- magic8Ball.py
+++ magic8Ball.py
@@ -4,7 +4,7 @@ def getAnswer(answerNumber):
   if answerNumber == 1:
     return 'It is certain'
   elif answerNumber == 2:
-    return 'It is dicedly so'
+    return 'It is decidedly so'
   elif answerNumber == 3:
     return 'Yes'
   elif answerNumber == 4:
